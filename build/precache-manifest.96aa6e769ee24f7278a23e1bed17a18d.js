930bf4ea8731e437d956ac955e59f559c38da53b
yyfc <592394158@qq.com>
2020-08-02 15:56:02 +0800

    commit build

---
+++ build/precache-manifest.96aa6e769ee24f7278a23e1bed17a18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c676e8b8a160b337a2e4c5b6d65df517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164a91b076a543f898c6",
+    "url": "/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "a210a2cf5b8f2a8729a5",
+    "url": "/static/css/main.235588f6.chunk.css"
+  },
+  {
+    "revision": "164a91b076a543f898c6",
+    "url": "/static/js/2.7b7e4b01.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.7b7e4b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a210a2cf5b8f2a8729a5",
+    "url": "/static/js/main.32e2196d.chunk.js"
+  },
+  {
+    "revision": "3b6b636a22583c22c585",
+    "url": "/static/js/runtime-main.2697b814.js"
+  }
+]);
